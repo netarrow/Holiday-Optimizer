89c7e4b591b0ed548c59206d20fa798e443e656c
Matteo <>
2021-08-18 12:17:31 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import ListView from "./ListView";
+import CalendarView from './CalendarView'
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === "List") {
+                iconName = focused
+                  ? "list"
+                  : "list"
+              } else if (route.name === "Calendar") {
+                iconName = focused ? "calendar" : "calendar";
+              }
+
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: "tomato",
+            tabBarInactiveTintColor: "gray",
+          })}
+        >
+          <Tab.Screen name="List" component={ListView} />
+          <Tab.Screen name="Calendar" component={CalendarView} />
+        </Tab.Navigator>
+      </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
---
+++ ListView.js
@@ -0,0 +1,11 @@
+
+import * as React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function ListView() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>List View!</Text>
+      </View>
+    );
+  }
