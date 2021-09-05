cc1c64cef862db84cb280f8333eb476a871d187b
Matteo <>
2021-09-05 12:11:56 +0200

    fix flex style to avoid android crash. change days order

--- src/SettingsView.js
+++ src/SettingsView.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import { Text, View, Switch } from "react-native";
 
 const days = [
-  { day: "Domenica", id: 0 },
   { day: "Lunedì", id: 1 },
   { day: "Martedì", id: 2 },
   { day: "Mercoledì", id: 3 },
   { day: "Giovedì", id: 4 },
   { day: "Venerdì", id: 5 },
   { day: "Sabato", id: 6 },
+  { day: "Domenica", id: 0 }
 ];
 
 export default function SettingsView() {
@@ -25,38 +25,23 @@ export default function SettingsView() {
     setweDays(newArray);
   };
   return (
-     <View>
+     <View style={{ marginTop: 20, marginLeft: 20 }}>
       <Text style={{ marginBottom: 20 }}>
         Indica quali sono i giorni della settimana dove non lavori normalmente
       </Text>
       {days.map((item) => {
         return (
-          <View
-            key={item.day}
-            style={{
-              alignItems: "top",
-              justifyContent: "flex-start",
-              flexDirection: "row",
-            }}
-          >
-            <View
-              style={{
-                flex: 1,
-                alignSelf: "center",
-                justifyContent: "flex-evenly",
-              }}
-            >
-              <Text>{item.day}</Text>
+          <View key={item.id} 
+                style={{
+                marginBottom: 5,
+                flexDirection: "row"
+              }}>
+            <View style={{flex: 1, alignSelf: 'center'}}>
+              <Text style={{alignSelf:"center"}}>{item.day}</Text>
             </View>
-            <View
-              style={{
-                flex: 4,
-                alignSelf: "center",
-                justifyContent: "space-evenly",
-                paddingBottom: 5,
-              }}
-            >
-              <Switch
+            <View style={{flex: 4}}>
+              <Switch 
+                style={{alignSelf:"flex-start"}}
                 trackColor={{ false: "#767577", true: "#81b0ff" }}
                 thumbColor={weDays.includes(item.id) ? "#f5dd4b" : "#f4f3f4"}
                 ios_backgroundColor="#3e3e3e"
